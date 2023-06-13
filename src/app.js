78c94613e08a08ccab25475795f814529fac4ca6
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2023-06-13 23:01:50 +0200

    connection with mongodb added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
-const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-console.log(uuidv4())
+mongoose.set('strictQuery', false);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
+
+app.post('/', (req, res) => {
+    res.send('this is post req')
+})
+
+
+app.post('/api/customers', (req, res) => {
+    console.log(req.body)
+})
+
+const start = async () =>  {
+    try{
+        await mongoose.connect(CONNECTION);
+        
+        app.listen(PORT, () => {
+            console.log(`app listening on port ${PORT}!`)
+        })
+    }catch(err){
+        console.log(err.message)
+    }
+}
+
+start();
